a562b40d135b28803328969bafd61214e2760d08
Вероника <@WIN-BBNSI5FE1UN>
2024-03-02 15:03:25 +0500

    Добавьте файлы проекта.

---
+++ Gyl/Gyl.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream> 
+#include <Windows.h>
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int numbers[7];
+    int maxNum;
+
+ 
+    for (int i = 0; i < 7; i++) {
+        std::cout << "Введите число " << (i + 1) << ": ";
+        std::cin >> numbers[i];
+    }
+
+    
+    maxNum = numbers[0];
+    for (int i = 1; i < 7; i++) 
+    {
+        if (numbers[i] > maxNum) 
+        {
+            maxNum = numbers[i];
+        }
+    }
+
+    
+    std::cout << "Максимальное число: " << maxNum;
+
+    return 0;
+}
